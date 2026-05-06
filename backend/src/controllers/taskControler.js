7a0f0d66b6dc777631e1a491ef5bd914d6e9a510
Duyquang2504 <quang250412@gmail.com>
2026-05-06 15:47:55 +0700

    feat: implement CRUD APIs and connect MongoDB

---
+++ backend/src/controllers/taskControler.js
@@ -0,0 +1,60 @@
+import Task from "../models/Task.js";
+
+export const getAllTasks = async (req, res) => {
+  try {
+    const task = await Task.find().sort({ createdAt: -1 });
+
+    res.status(200).json(task);
+  } catch (error) {
+    console.error("Lỗi khi gọi getAllTasks", error);
+    res.status(500).json({ message: "Lỗi hệ thống" });
+  }
+};
+export const createTask = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const addTask = new Task({ title, description });
+    const newTask = await addTask.save();
+    res.status(200).json(newTask);
+  } catch (error) {
+    console.error("Lỗi khi gọi createTasks", error);
+    res.status(500).json({ message: "Lỗi hệ thống" });
+  }
+};
+
+export const updateTask = async (req, res) => {
+  try {
+    const { title, description, status, completedAt } = req.body;
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      {
+        title,
+        description,
+        status,
+        completedAt,
+      },
+      { new: true },
+    );
+    if (!updatedTask) {
+      return res.status(500).json({ message: "Nhiệm vụ không tồn tại !" });
+    }
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    console.error("Lỗi khi gọi updateTask", error);
+    res.status(500).json({ message: "Lỗi hệ thống !" });
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  try {
+    const deleteTask = await Task.findByIdAndDelete(req.params.id);
+    if (!deleteTask) {
+      return res.status(500).json({ message: "Nhiệm vụ không tồn tại" });
+    }
+    res.status(200).json(deleteTask);
+  } catch (error) {
+    console.error("Lỗi khi gọi deleteTask", error);
+    return res.status(500).json({ message: "Lỗi hệ thống" });
+  }
+  res.status(201).json({ message: "Task đã được xóa" });
+};
